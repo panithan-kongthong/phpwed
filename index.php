732cd0a416d89647f6a3c0b8d18d51a81c20fb13
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2020-11-13 15:18:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,44 @@
+<html>
+<head>
+<title>ITF Lab</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'hololiveth.mysql.database.azure.com', 'panithan@hololiveth', 'folk_zaza2020', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-
-
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
 mysqli_close($conn);
 ?>
+</body>
+</html>
