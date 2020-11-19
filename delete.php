24cf02e25153048c8c93af56e6844a4af971045b
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2020-11-19 20:33:05 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,34 @@
 <?php
-
 $conn = mysqli_connect('hololiveth.mysql.database.azure.com', 'panithan@hololiveth', 'folk_zaza2020', 'ITFLab', 3306);
-$sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'';
+$sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
 
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-if (mysqli_query($conn, $sql)) {
-    echo "Successfully."; 
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5 text-center">
+
+        <h3>
+            <?php
+            if (mysqli_query($conn, $sql)) {
+                echo "Delete Completed";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
+            ?>
+        </h3>
+        <a href="index.php" class="btn btn-light mt-3">BACK</a>
+
+    </div>
+</body>
+
+</html>
