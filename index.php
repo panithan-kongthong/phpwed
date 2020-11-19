a2d05bda52706bb368b9211101f5e3d92780e960
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2020-11-19 21:07:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                 <td><?php echo $Result['Comment']; ?></td>
                 <td><?php echo $Result['Link']; ?></td>
                 <td>
-                  <a class="btn btn-success" href="edit.php?ID=<?php echo $Result['ID']; ?>">EDIT</a>
+                  <a class="btn btn-Primary" href="edit.php?ID=<?php echo $Result['ID']; ?>">EDIT</a>
                   <a class="btn btn-danger" href="delete.php?ID=<?php echo $Result['ID']; ?>">DELETE</a>
                 </td>
               </tr>
