1c68a32f3754a4da5bf78aac6a54c862137c2fe2
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2020-11-19 10:35:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div><form method="post"> 
-        <input type="submit" name="button1"
-                value="Button1"/> 
-          
-        <input type="submit" name="button2"
-               value="Button2"/></from> </th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -31,7 +26,12 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['Link'];?><form method="post"> 
+        <input type="submit" name="button1"
+                value="Button1"/> 
+          
+        <input type="submit" name="button2"
+               value="Button2"/></form></td>
   </tr>
 <?php
 }
